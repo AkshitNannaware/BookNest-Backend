ce24b547a0283d3cfe3d78294ff6185e8427449f
Akshit <akshitnannaware@gmail.com>
2025-09-24 17:16:04 +0530

    Initial commit

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.use("/api", studentRoutes);
 
 
 // Environment variables
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bookenest';
 // MongoDB connection
 mongoose
